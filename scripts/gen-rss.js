3802387ba32cb3bb53ee2efea6ed49a51d8d2eae
alvian <alvian@metalit.id>
2022-08-04 21:01:39 +0700

    fix rss

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -17,13 +17,13 @@ async function generate() {
       if (name.startsWith('index.')) return
 
       const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
+        path.join(__dirname, '..', 'pages', 'journals', name)
       )
       const frontmatter = matter(content)
 
       feed.item({
         title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
+        url: '/journals/' + name.replace(/\.mdx?/, ''),
         date: frontmatter.data.date,
         description: frontmatter.data.description,
         categories: frontmatter.data.tag.split(', '),
